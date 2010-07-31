89a031c67e70e182691801765ddec2f09e73b621
James S <i@nixeagle.org>
2010-07-31 00:43:21 +0000

    Move older generic type dispatch out of instruction.lisp

--- src/instruction.lisp
+++ src/instruction.lisp
@@ -40,43 +40,6 @@
 
 
 
-(defstruct dispatch-entry
-  "Contains required TYPE and PRIORITY that FUNCTION is called.
-
-Meant for use in a hashtable with a list of these matching a particular
-function name so that dispatch based on _types_ can happen. By default we
-just do `typep' on the function's arguments, but for more specialized
-checking the function TEST-FUNCTION can be modified to return t when
-FUNCTION should be called.
-
-TEST-FUNCTION is a function of one argument, that of the prospective
-arguments to check types on that returns t or nil if those arguements are
-of a valid type."
-  (type (assert-value-supplied))
-  (test-function nil :type (or null nutils:function-designator))
-  (priority 0 :type fixnum)
-  (function (assert-value-supplied) :type nutils:function-designator))
-
-(defun dispatch-entry-equal (entry1 entry2)
-  "Compare two entries based on required type alone."
-  (declare (dispatch-entry entry1 entry2))
-  (equal (dispatch-entry-type entry1)
-         (dispatch-entry-type entry2)))
-
-
-(defun sortf-type-dispatch-entries (name dispatch-table)
-  "Sort functions under NAME in DISPATCH-TABLE by priority."
-  (setf (gethash name dispatch-table)
-        (sort (gethash name dispatch-table)
-              #'> :key #'dispatch-entry-priority)))
-
-(defun tuncall (name table &rest args)
-  (declare (optimize (speed 3) (safety 0))
-           (dynamic-extent args))
-  (apply (the function (loop for entry in (gethash name table)
-                          when (typep args (dispatch-entry-type entry))
-                          return (dispatch-entry-function entry)))
-         args))
 
 (defun build-cons-type (list)
   (if list
